ab892e9a10840fdd27f76d1134bae6193dc107d9
wujiangbo.aerozhonghuan.com <wujiangbo@aerozhonghuan.com>
2017-05-19 13:25:13 +0800

    commit

---
+++ src/main/java/com/mapbar/display/common/Const.java
@@ -0,0 +1,14 @@
+package com.mapbar.display.common;
+
+/**
+ * 常量类
+ * @Author: wujiangbo
+ * @Create: 2017/05/19 13:11
+ */
+public class Const {
+
+    public static final String RESP_REALTIME_POSITION_TYPE = "FeatureCollection";
+
+    public static final String RESP_FEATURE_TYPE = "Feature";
+
+}
