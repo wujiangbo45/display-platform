70ddc7402c0bf80339b8ddcea9329afce66260c2
wujiangbo.aerozhonghuan.com <wujiangbo@aerozhonghuan.com>
2017-05-22 08:49:09 +0800

    commit

---
+++ src/main/java/com/mapbar/display/dto/BaseResp.java
@@ -0,0 +1,17 @@
+package com.mapbar.display.dto;
+
+/**
+ * @Author: wujiangbo
+ * @Create: 2017/05/22 8:48
+ */
+public class BaseResp {
+    private int code;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+}
