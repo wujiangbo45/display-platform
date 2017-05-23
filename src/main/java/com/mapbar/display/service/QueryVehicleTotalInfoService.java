82121eebb1eae60963e8f767210348ab75db886f
lxwangjiachen <w890118!>
2017-05-23 14:56:50 +0800

    查询车辆统计信息

--- src/main/java/com/mapbar/display/service/QueryVehicleTotalInfoService.java
+++ src/main/java/com/mapbar/display/service/QueryVehicleTotalInfoService.java
@@ -1,8 +1,10 @@
 package com.mapbar.display.service;
 
+import com.mapbar.display.aspect.LoggerAspect;
 import com.mapbar.display.common.CommonDao;
 import com.mapbar.display.dto.GetTotalMileageAndPackage;
 import com.mapbar.display.service.base.BaseService;
+import org.aspectj.lang.JoinPoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,16 +22,15 @@ public class QueryVehicleTotalInfoService extends BaseService{
     private static final Logger logger = LoggerFactory.getLogger(QueryVehicleTotalInfoService.class);
     @Autowired
     public CommonDao dao;
+    LoggerAspect loggerAspect;
     @Transactional
     public int getTotalVehicle(){
-        logger.info("============getTotalVehicle Start===========");
         int intTotal = 0;
 
         String countSql = sqlLaberUtil.getSqlLabel("carTotal");
         int count = Integer.parseInt(entityManager.createNativeQuery(countSql).getSingleResult().toString());
         intTotal = count;
 
-        logger.info("============getTotalVehicle End===========");
         return intTotal;
     }
 }
