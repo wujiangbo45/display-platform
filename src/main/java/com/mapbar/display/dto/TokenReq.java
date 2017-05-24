068db596b5654a5c90855c1a96ee8b0c78e68270
wujiangbo.aerozhonghuan.com <wujiangbo@aerozhonghuan.com>
2017-05-24 11:49:00 +0800

    commit

--- src/main/java/com/mapbar/display/dto/TokenReq.java
+++ src/main/java/com/mapbar/display/dto/TokenReq.java
@@ -1,11 +1,17 @@
 package com.mapbar.display.dto;
 
+import org.hibernate.validator.constraints.NotBlank;
+
+import javax.validation.constraints.NotNull;
+
 /**
  * @Author: wujiangbo
  * @Create: 2017/05/24 11:17
  */
 public class TokenReq {
 
+    @NotNull(message = "token不能为空")
+    @NotBlank(message = "token不能为空")
     protected String token;
 
     public String getToken() {
