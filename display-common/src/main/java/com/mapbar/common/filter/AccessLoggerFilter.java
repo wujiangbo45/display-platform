6b456e10ba61220e2f9d8158466dbbf9a24e66b1
wujiangbo.aerozhonghuan.com <wujiangbo@aerozhonghuan.com>
2017-05-26 11:48:39 +0800

    commit

--- display-common/src/main/java/com/mapbar/common/filter/AccessLoggerFilter.java
+++ display-common/src/main/java/com/mapbar/common/filter/AccessLoggerFilter.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -37,7 +38,8 @@ public class AccessLoggerFilter implements Filter {
 	public void doFilter(ServletRequest request, ServletResponse response,
 			FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest httpRequest = (HttpServletRequest)request;
-
+		HttpServletResponse httpServletResponse = (HttpServletResponse) response;
+		httpServletResponse.setHeader("Access-Control-Allow-Origin", "*");
 		String actionMethod = httpRequest.getParameter(actionParamName);
 		String uri = httpRequest.getRequestURI();
 		String httpMethod = httpRequest.getMethod();
