ce79123b22d0dfbf64ea590143ce66a3bad5b919
wujiangbo45 <285874318@qq.com>
2017-09-29 16:31:25 +0800

    commit

---
+++ display-business/src/main/java/com/mapbar/display/dto/ServiceWorkOrderResp.java
@@ -0,0 +1,143 @@
+package com.mapbar.display.dto;
+
+/**
+ * @Author: wujiangbo
+ * @Create: 2017/09/29 下午4:21
+ */
+public class ServiceWorkOrderResp {
+    private String todalOutService;
+    private String outService;
+    private String totalReservationOrder;
+    private String reservationOrder;
+    private String totalIndependentStation;
+    private String independentStation;
+    private String totalWorkOrder;
+    private String beAssignedOrder;
+    private String bePickedOrder;
+    private String checkingOrder;
+    private String doingServiceOrder;
+    private String beConfirmedOrder;
+    private String waitingOutStationOrder;
+    private String outStationOrder;
+    private String closedOrder;
+
+    public String getTodalOutService() {
+        return todalOutService;
+    }
+
+    public void setTodalOutService(String todalOutService) {
+        this.todalOutService = todalOutService;
+    }
+
+    public String getOutService() {
+        return outService;
+    }
+
+    public void setOutService(String outService) {
+        this.outService = outService;
+    }
+
+    public String getTotalReservationOrder() {
+        return totalReservationOrder;
+    }
+
+    public void setTotalReservationOrder(String totalReservationOrder) {
+        this.totalReservationOrder = totalReservationOrder;
+    }
+
+    public String getReservationOrder() {
+        return reservationOrder;
+    }
+
+    public void setReservationOrder(String reservationOrder) {
+        this.reservationOrder = reservationOrder;
+    }
+
+    public String getTotalIndependentStation() {
+        return totalIndependentStation;
+    }
+
+    public void setTotalIndependentStation(String totalIndependentStation) {
+        this.totalIndependentStation = totalIndependentStation;
+    }
+
+    public String getIndependentStation() {
+        return independentStation;
+    }
+
+    public void setIndependentStation(String independentStation) {
+        this.independentStation = independentStation;
+    }
+
+    public String getTotalWorkOrder() {
+        return totalWorkOrder;
+    }
+
+    public void setTotalWorkOrder(String totalWorkOrder) {
+        this.totalWorkOrder = totalWorkOrder;
+    }
+
+    public String getBeAssignedOrder() {
+        return beAssignedOrder;
+    }
+
+    public void setBeAssignedOrder(String beAssignedOrder) {
+        this.beAssignedOrder = beAssignedOrder;
+    }
+
+    public String getBePickedOrder() {
+        return bePickedOrder;
+    }
+
+    public void setBePickedOrder(String bePickedOrder) {
+        this.bePickedOrder = bePickedOrder;
+    }
+
+    public String getCheckingOrder() {
+        return checkingOrder;
+    }
+
+    public void setCheckingOrder(String checkingOrder) {
+        this.checkingOrder = checkingOrder;
+    }
+
+    public String getDoingServiceOrder() {
+        return doingServiceOrder;
+    }
+
+    public void setDoingServiceOrder(String doingServiceOrder) {
+        this.doingServiceOrder = doingServiceOrder;
+    }
+
+    public String getBeConfirmedOrder() {
+        return beConfirmedOrder;
+    }
+
+    public void setBeConfirmedOrder(String beConfirmedOrder) {
+        this.beConfirmedOrder = beConfirmedOrder;
+    }
+
+    public String getWaitingOutStationOrder() {
+        return waitingOutStationOrder;
+    }
+
+    public void setWaitingOutStationOrder(String waitingOutStationOrder) {
+        this.waitingOutStationOrder = waitingOutStationOrder;
+    }
+
+    public String getOutStationOrder() {
+        return outStationOrder;
+    }
+
+    public void setOutStationOrder(String outStationOrder) {
+        this.outStationOrder = outStationOrder;
+    }
+
+    public String getClosedOrder() {
+        return closedOrder;
+    }
+
+    public void setClosedOrder(String closedOrder) {
+        this.closedOrder = closedOrder;
+    }
+}
