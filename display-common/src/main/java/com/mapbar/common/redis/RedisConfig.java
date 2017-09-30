de3369eb9c1bb47835c1a57647969050d1d290bd
wujiangbo45 <285874318@qq.com>
2017-09-30 11:43:14 +0800

    commit

--- display-common/src/main/java/com/mapbar/common/redis/RedisConfig.java
+++ display-common/src/main/java/com/mapbar/common/redis/RedisConfig.java
@@ -18,44 +18,19 @@ import redis.clients.jedis.JedisPoolConfig;
 //@Configuration
 //@EnableConfigurationProperties(RedisProperties.class)
 public class RedisConfig {
-//
-//    @Autowired
-//    private RedisProperties redisProperties;
-//    @Bean
-//    JedisConnectionFactory jedisConnectionFactory() {
-//        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
-//        jedisConnectionFactory.setHostName(redisProperties.getHost());
-//        jedisConnectionFactory.setPort(redisProperties.getPort());
-//        jedisConnectionFactory.setPassword(redisProperties.getPassword());
-//        jedisConnectionFactory.setDatabase(redisProperties.getDatabase());
-//        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-//        jedisPoolConfig.setMinIdle(redisProperties.getPool().getMinIdle());
-//        jedisPoolConfig.setMaxIdle(redisProperties.getPool().getMaxIdle());
-//        jedisPoolConfig.setMaxTotal(redisProperties.getPool().getMaxActive());
-//        jedisPoolConfig.setMaxWaitMillis(redisProperties.getPool().getMaxWait());
-//        jedisConnectionFactory.setPoolConfig(jedisPoolConfig);
-//        return jedisConnectionFactory;
-//    }
+
+//   没有啊
 //
 //    @Bean
-//    public RedisTemplate<String,String> redisTemplate(JedisConnectionFactory jedisConnectionFactory) {
-//        RedisTemplate<String,String> template = new RedisTemplate<String,String>();
-//        template.setConnectionFactory(jedisConnectionFactory);
-//        template.setKeySerializer(new StringRedisSerializer());
-//        template.setValueSerializer(new StringRedisSerializer());
-//        return template;
+//    public JedisPool jedis(){
+//        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
+//        genericObjectPoolConfig.setMaxIdle(redisProperties.getPool().getMaxIdle());
+//        genericObjectPoolConfig.setMaxTotal(redisProperties.getPool().getMaxTotal());
+//        genericObjectPoolConfig.setMinIdle(redisProperties.getPool().getMinIdle());
+//
+//        JedisPool jedisPool = new JedisPool(genericObjectPoolConfig,redisProperties.getHost(),redisProperties.getPort());
+//        return jedisPool;
 //    }
-////
-////    @Bean
-////    public JedisPool jedis(){
-////        GenericObjectPoolConfig genericObjectPoolConfig = new GenericObjectPoolConfig();
-////        genericObjectPoolConfig.setMaxIdle(redisProperties.getPool().getMaxIdle());
-////        genericObjectPoolConfig.setMaxTotal(redisProperties.getPool().getMaxTotal());
-////        genericObjectPoolConfig.setMinIdle(redisProperties.getPool().getMinIdle());
-////
-////        JedisPool jedisPool = new JedisPool(genericObjectPoolConfig,redisProperties.getHost(),redisProperties.getPort());
-////        return jedisPool;
-////    }
 
 
 }
